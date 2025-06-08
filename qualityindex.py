db2a7ce59c17a4aa939fcd209cb5fac59d53e2ea
owlpotato <114093012+owlpotato@users.noreply.github.com>
2025-06-09 01:59:02 +0200

    Update and rename testAPI.py to qualityindex.py
    started to develop the quality index (will be using an decision tree, developed the basic framework, need to incorporate basic ml into it)

---
+++ qualityindex.py
@@ -0,0 +1,36 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+
+class qualityindex:
+    def __init__(self, h_index, recency, number_of_citations, journal_impact):
+        self.h_index = h_index
+        self.recency = recency
+        self.number_of_citations = number_of_citations
+        self.journal_impact = journal_impact
+        self.h_index_para = 10 #more than or equal to
+        self.number_of_citations_para = 10 #more than or equal to
+        self.recency_para = 5 ##less than or equal to 5 years
+        self.journal_impact_para = 2##more than or equal to
+
+    def update_h_index(self, bool):
+        if bool == true
+            
+    def update_h_index(self, bool):
+        if 
+    def update_h_index(self, bool):
+        if 
+    def update_h_index(self, bool):
+        if 
+
+    def __decision_tree___(self, other):
+        if h_index < h_index_para:
+            return f"h_index error"
+        if number_of_citations < number_of_citations_para:
+            return f"number_of_citations error"
+        if journal_impact < journal_impact_para:
+            return f"journal_impact error"
+        if recency > recency_para:
+            return f"recency error"
+
+##nothing
--- testAPI.py
+++
@@ -1,9 +0,0 @@
-class TestAPI:
-    def __init__(self, variable, name):
-        self.name = name
-        self.variable = variable
-
-    def __eq__(self, other):
-        return self.variable == other.variable
-
-##nothing
